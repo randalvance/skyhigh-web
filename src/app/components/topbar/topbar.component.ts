521fd176261e40e3d031a2b0e991c7a8e4d3c15a
Randal Cunanan <randal@randalvance.net>
2017-05-23 04:14:36 -0400

    Adding redux dev tools

--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,9 +1,10 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.css']
+  styleUrls: ['./topbar.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TopBarComponent implements OnInit {
 
--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppActions, AppReducers } from './reducers';
+import { environment } from '../../environments/environment';
+
+const optionalImports = [];
+
+if (!environment.production) {
+  optionalImports.push(StoreDevtoolsModule.instrumentOnlyWithExtension());
+}
 
 @NgModule({
   imports:  [
-    StoreModule.provideStore(AppReducers)
+    StoreModule.provideStore(AppReducers),
+    ...optionalImports
   ],
   declarations: [],
   exports: [],
