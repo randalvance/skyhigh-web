7ba380217a0b58f5aad75e57387fdd9ff6deb9f9
Randal Cunanan <randal@randalvance.net>
2017-06-11 18:11:13 +0800

    Showing subjects in enrollment list

--- src/app/enrollment/enrollment-list.component.ts
+++ src/app/enrollment/enrollment-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import { Store } from '@ngrx/store';
@@ -8,10 +8,37 @@ import { PageComponentBase } from '../shared';
 import { Enrollment } from '.';
 import { EnrollmentService } from './enrollment.service';
 
+import { animate, trigger, transition, state, style } from '@angular/animations';
+
 import 'rxjs/add/operator/takeUntil';
 
 @Component({
-  templateUrl: 'enrollment-list.component.html'
+  templateUrl: 'enrollment-list.component.html',
+  styleUrls: ['enrollment-list.component.scss'],
+  animations: [
+    trigger('subjectRowAnimate', [
+      transition(':enter', [
+        style({
+          transform: 'translateY(-100px)',
+          opacity: 0
+        }),
+        animate('300ms', style({
+          transform: 'translateY(0)',
+          opacity: 1
+        }))
+      ]),
+      transition(':leave', [
+        style({
+          transform: 'translateY(0)',
+          opacity: 1
+        }),
+        animate('300ms', style({
+          transform: 'translateY(-100px)',
+          opacity: 0
+        }))
+      ])
+    ])
+  ]
 })
 export class EnrollmentListComponent extends PageComponentBase implements OnInit, OnDestroy {
 
@@ -25,11 +52,20 @@ export class EnrollmentListComponent extends PageComponentBase implements OnInit
   }
 
   ngOnInit(): void {
-    this.enrollments$ = this.enrollmentService.getAll();
+    this.enrollments$ = this.enrollmentService.getAll().map(enrollments => enrollments.map((enrollment: any) => {
+
+      enrollment.subjectsShown = false;
+
+      return <Enrollment>enrollment;
+    }));
   }
 
   ngOnDestroy(): void {
-      this.ngUnsubscribe.next();
-      this.ngUnsubscribe.complete();
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+
+  toggleSubjects(enrollment: any) {
+    enrollment.subjectsShown = !enrollment.subjectsShown;
   }
 }
