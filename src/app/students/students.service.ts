cc05dcadd81dc1935bd9d319eacf0911816adb5d
Randal Cunanan <randal@randalvance.net>
2017-06-11 23:24:59 +0800

    Update url to student service

--- src/app/students/students.service.ts
+++ src/app/students/students.service.ts
@@ -7,6 +7,9 @@ import { Observable, ObservableInput } from 'rxjs/Observable';
 @Injectable()
 export class StudentsService extends ResourceServiceBase<Student> {
   constructor(http: Http) {
-    super(http, 'http://localhost:5000/api/', 'students');
+    // TODO: Get this from environment variable
+    // We might need to repackage this app in a node container instead of nginx
+    // Note: This needs to be the url of how we will access it from the browser, otherwise you will get DNS errors
+    super(http, 'http://localhost:8181/api/', 'students');
   }
 }
